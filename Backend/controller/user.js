ba958b66250aeb8715ee6b5e57d68ed16b5c4f09
hemant069 <hemantprajapati20500@gmail.com>
2025-01-26 19:15:44 +0530

    added the profile imgaes in user model

--- Backend/controller/user.js
+++ Backend/controller/user.js
@@ -1,12 +1,22 @@
 const userModel = require("../model/userModel");
+const bcrypt = require("bcrypt");
 
 const handleSignUp = async (req, res) => {
   try {
     const { username, email, password } = req.body;
+    const saltround = 10;
+    bcrypt.hash(password, saltround, async (err, hashPass) => {
+      if (err) {
+        return res.json({
+          msg: "Something went wrong with hashing password",
+          err,
+        });
+      }
 
-    const user = await userModel({ username, email, password });
+      const user = await userModel({ username, hashPass, email });
 
-    user.save();
+      user.save();
+    });
 
     res.status(201).json({ msg: "user is created successfully", user });
   } catch (error) {
--- Backend/model/userModel.js
+++ Backend/model/userModel.js
@@ -4,6 +4,8 @@ const userSchema = mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  profileImg: { type: String, default: "./images/default.png" },
+  role: { type: String, enum: ["USER", "ADMIN"], default: "USER" },
 });
 
 const userModel = mongoose.model("users", userSchema);
