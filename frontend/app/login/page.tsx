b69d919f925085e5c12d8eee640fa87228acb550
hemant069 <hemantprajapati20500@gmail.com>
2025-02-05 19:13:09 +0530

    gif is added to login page

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -4,6 +4,9 @@ import { Input } from "@/components/ui/input";
 import Link from "next/link";
 import React from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
+import logingif from "../../public/login.gif";
+import typing from "../../public/typing.gif";
+import Image from "next/image";
 
 interface submitData {
   email: string;
@@ -15,11 +18,22 @@ const page = () => {
 
   const handleLogin: SubmitHandler<submitData> = (data: submitData) => {};
   return (
-    <div className="">
-      <div className="text-center">
-        <h1>Login Page</h1>
-      </div>
-      <div className="flex justify-center flex-col">
+    <div className="flex flex-col justify-center items-center h-screen    ">
+      <div className="flex justify-center gap-4 flex-col shadow-2xl rounded-xl   p-24 w-[45rem] ">
+        <div className="flex justify-center gap-11 ">
+          <Image
+            className="rounded-full "
+            width={140}
+            src={logingif}
+            alt="login-gif"
+          />
+          <Image
+            className="rounded-full "
+            width={140}
+            src={typing}
+            alt="typing-gif"
+          />
+        </div>
         <Input type="email" {...register("email")} placeholder="Enter email" />
         <Input
           type="password"
@@ -27,9 +41,14 @@ const page = () => {
           placeholder="Enter password"
         />
         <Button onClick={handleSubmit(handleLogin)}>Login</Button>
-        <Link href={"/signup"}>
-          <p className="text-blue-500">Create a account</p>
-        </Link>
+        <div className="flex justify-around">
+          <Link href={"#"}>
+            <p className="text-blue-500">Forget Password</p>
+          </Link>
+          <Link href={"/signup"}>
+            <p className="text-blue-500">Create a account</p>
+          </Link>
+        </div>
       </div>
     </div>
   );
