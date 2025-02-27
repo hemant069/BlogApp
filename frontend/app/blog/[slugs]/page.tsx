94ae6fe38297aceecc0dcfc40c64ab8c714c54d8
hemant069 <hemantprajapati20500@gmail.com>
2025-02-27 22:36:38 +0530

    blog page is added with data

--- frontend/app/blog/[slugs]/page.tsx
+++ frontend/app/blog/[slugs]/page.tsx
@@ -1,7 +1,64 @@
-import React from "react";
+"use client";
+import axios from "axios";
+import { usePathname, useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import Cookies from "js-cookie";
+import Image from "next/image";
 
+interface BlogType {
+  title: string;
+  content: string;
+  coverImgUrl: string;
+}
 const page = () => {
-  return <div>Blog Page</div>;
+  const pathname = usePathname();
+  const id = pathname.split("/")[2];
+
+  const [blog, setBlog] = useState<BlogType | undefined>();
+
+  const token = Cookies.get("token");
+
+  const handleBlog = async () => {
+    try {
+      const res = await axios.get(
+        `${process.env.NEXT_PUBLIC_BACKEND}blog/${id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const data = res?.data?.post;
+      setBlog(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    handleBlog();
+  }, [pathname]);
+
+  // console.log(blog);
+
+  return (
+    <div className="flex justify-center flex-col">
+      <div>
+        <Image
+          src={blog?.coverImgUrl}
+          width={200}
+          height={200}
+          alt={blog?.coverImgUrl}
+        />
+      </div>
+      <div>
+        <p>{blog?.title}</p>
+      </div>
+      <div>
+        <p>{blog?.content}</p>
+      </div>
+    </div>
+  );
 };
 
 export default page;
