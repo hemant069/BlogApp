f834fbb6b12131c803d64bc45592861c8e793fcd
hemant069 <hemantprajapati20500@gmail.com>
2025-03-20 18:08:30 +0530

    added the comment

--- Backend/controller/comment.js
+++ Backend/controller/comment.js
@@ -2,12 +2,29 @@ const CommentModel = require("../model/CommetModel");
 
 const handleComment = async (req, res) => {
   try {
-    const { content, userId, blogId } = req.body;
+    const { content, userId, blogId, parentcommentId } = req.body;
 
-    const comment = new CommentModel({ content, user: userId, blog: blogId });
-    await comment.save();
-    console.log(comment);
-    return res.json({ msg: "comment successfully", data: comment });
+    if (!parentcommentId) {
+      const comment = new CommentModel({ content, user: userId, blog: blogId });
+      await comment.save();
+      return res.json({ msg: "comment successfully", data: comment });
+    } else {
+      const reply = new CommentModel({
+        content,
+        user: userId,
+        blog: blogId,
+        parentComment: parentcommentId,
+      });
+      await reply.save();
+
+      await CommentModel.findByIdAndUpdate(
+        parentcommentId,
+        { $push: { replies: reply._id } },
+        { new: true }
+      );
+
+      return res.json({ msg: "comment reply added success", data: reply });
+    }
   } catch (error) {
     return res.json({
       msg: "Something went wrong with handleComment",
