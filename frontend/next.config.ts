1af064265404da1e4f731f4192737703f47d0e67
hemant069 <hemantprajapati20500@gmail.com>
2025-10-13 13:24:29 +0530

    changes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
@@ -11,11 +11,10 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
-        pathname: '/**', // for Google profile images
+        pathname: '/**',
       },
     ],
   },
-  output: 'export',
 };
 
 export default nextConfig;
